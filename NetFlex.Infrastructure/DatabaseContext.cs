1feed035567e0c01c0d2650bef2f2c40bff42a3e
Kenneth Gibson <scaphoidederps@gmail.com>
2022-03-07 11:03:02 +0300

    Add project files.

---
+++ NetFlex.Infrastructure/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetFlex.Core.Models;
+
+namespace NetFlex.Infrastructure
+{
+	public class DatabaseContext : DbContext
+	{
+		public DbSet<User> Users { get; set; }
+		public DatabaseContext(DbContextOptions options)
+			: base(options)
+		{
+			Database.EnsureCreated();
+		}
+	}
+}
