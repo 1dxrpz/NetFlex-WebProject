51603291c55a57701a56736d71ae849897c26e3f
Kenneth Gibson <scaphoidederps@gmail.com>
2022-03-17 21:38:51 +0300

    feature (VideoPlayer): added custom player to site

---
+++ NetFlex.WEB/ViewModels/VideoPlayerViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace NetFlex.WEB.ViewModels
+{
+	public class VideoPlayerViewModel
+	{
+		public string? VideoTitle { get; set; }
+		public string? EpisodeName { get; set; }
+		public string? VideoLink { get; set; }
+	}
+}
